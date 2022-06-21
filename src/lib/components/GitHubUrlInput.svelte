e3490ee1fba1e43f4c5a2d9babdfd0f5a7f8bac8
Moritz <fullermoritz@gmail.com>
2022-06-21 16:25:00 +0200

    ✨ added input field for url

--- src/lib/components/GitHubUrlInput.svelte
+++ src/lib/components/GitHubUrlInput.svelte
@@ -1,9 +1,20 @@
 <script lang="ts">
-	let input = '';
+	export let input = '';
+
+	function extractRepoAndOwner(url) {
+		const match = url.match(/^(?:https?:\/\/)?(?:www\.)?github\.com\/([^/]+)\/([^/]+)(?:\/.*)?$/);
+		if (match) {
+			return {
+				owner: match[1],
+				repo: match[2]
+			};
+		}
+		return null;
+	}
 </script>
 
 <div class="flex justify-center gap-4">
 	<input bind:value={input} class="input input-bordered" placeholder="enter github url" />
 
-	<button class="btn"> get data </button>
+	<button class="btn" on:click={() => console.log(extractRepoAndOwner(input))}> get data </button>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,10 +3,12 @@
 	import Languages from '$lib/components/chaoss-metrics/common/contributions/languages/Languages.svelte';
 	import Close from '$lib/components/chaoss-metrics/common/time/time-to-close/Close.svelte';
 	import GitHubUrlInput from '$lib/components/GitHubUrlInput.svelte';
+
+	let input;
 </script>
 
 <div class="flex flex-col justify-center m-6">
-	<GitHubUrlInput />
+	<GitHubUrlInput bind:input />
 	<Forks />
 	<Languages />
 	<Close />
