80c81f1ad4c4047afff31a9ba91fa1314e531a04
Moritz <fullermoritz@gmail.com>
2022-06-27 16:57:20 +0200

    ✨ added selection for different burstiness metrics

--- src/lib/components/chaoss-metrics/common/time/burstiness/Burstiness.svelte
+++ src/lib/components/chaoss-metrics/common/time/burstiness/Burstiness.svelte
@@ -4,8 +4,31 @@
 	import BurstinessPRs from './BurstinessPRs.svelte';
 
 	export let repo, owner, date;
+	let selected;
+
+	let metrics = [
+		{
+			label: 'Issues',
+			component: BurstinessIssues
+		},
+		{
+			label: 'PRs',
+			component: BurstinessPRs
+		},
+		{
+			label: 'Discussions',
+			component: BurstinessDiscussions
+		}
+	];
 </script>
 
-<BurstinessDiscussions {repo} {owner} {date} />
-<BurstinessPRs {repo} {owner} {date} />
-<BurstinessIssues {repo} {owner} {date} />
+<div>
+	<p class="text-2xl">Burstiness</p>
+	<select bind:value={selected} class="select select-bordered">
+		{#each metrics as metric}
+			<option value={metric.component}>{metric.label}</option>
+		{/each}
+	</select>
+</div>
+
+<svelte:component this={selected} {repo} {owner} {date} />
